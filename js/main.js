ae225b22c3fb94779708657daf44bdde7e99f2c5
Alex <alex_billerey@hotmail.com>
2015-09-14 01:19:14 +0100

    css refactor

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
 
     function getRandom() {
-        var sectionDef = $('.hero_container');
-        var sectionN = sectionDef.find('section').length;
-        var random = Math.floor(Math.random() * ((sectionN - 1) + 1) + 1);
+        var sectionDef = $('.hero_container'),
+            sectionN = sectionDef.find('section').length,
+            random = Math.floor(Math.random() * ((sectionN - 1) + 1) + 1);
         $(sectionDef).find('section:nth-child(' + random + ')').addClass('openHero');
         $(sectionDef).mouseenter(function () {
             $(this).find('section').removeClass('openHero');
         });
     }
+    function toggleHeaderShadow() {
+        /*if(condition) {
+            alert('boo');
+        }*/
+    }
 
     setTimeout(getRandom,3);
+    toggleHeaderShadow();
 });
 
 
